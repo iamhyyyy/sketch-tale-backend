28351c3e28426bd5756238e2876b7868eafb9456
HarryNg59 <huyheo05092004@gmail.com>
2026-09-13 16:07:39 +0700

    edit json rÃes value

--- sketch_tale.Application/DTOs/BaseFieldDto.cs
+++ sketch_tale.Application/DTOs/BaseFieldDto.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace sketch_tale.Application.DTOs;
 
 public abstract class BaseFieldDto
 {
+    [JsonPropertyOrder(-1)]
     public Guid Id { get; set; }
+
     public DateTime CreatedAt { get; set; }
     public Guid CreateBy { get; set; }
     public DateTime? UpdatedAt { get; set; }
